988119476a0cc541871801d05e77a120290594f1
Vohz <barrejordy.cira@gmail.com>
2020-01-20 15:06:29 +0100

    add nodemon and init server node

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res){
+    res.send('Node MiHo')
+})
+
+let server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`${process.env.SERVER_NAME || 'API'} LISTENING ON PORT ${server.address().port}`)
+});
